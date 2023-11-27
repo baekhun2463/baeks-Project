063b9d667eed219f73772c0508e13ebe2b328f89
백훈 <baekhun2463@gmail.com>
2023-11-27 23:33:52 +0900

    이미지 최종 완료(base64)

--- src/main/java/baeksproject/project/item/service/ItemServiceV1.java
+++ src/main/java/baeksproject/project/item/service/ItemServiceV1.java
@@ -5,7 +5,6 @@ import baeksproject.project.item.repository.ItemRepository;
 import baeksproject.project.item.repository.ItemSearchCond;
 import baeksproject.project.item.repository.ItemUpdateDto;
 import baeksproject.project.login.domain.member.Member;
-import baeksproject.project.login.repository.JpaMemberRepositoryV1;
 import baeksproject.project.login.repository.MemberRespository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,11 +16,9 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
@@ -37,47 +34,52 @@ public class ItemServiceV1 implements ItemService {
 
 
     public Item saveItemWithMember(Long memberId, Item item, MultipartFile imageFile) {
-        Member member = memberRespository.findById(memberId).orElseThrow(() -> new RuntimeException("Member not found"));
+        Member member = memberRespository.findById(memberId)
+                .orElseThrow(() -> new RuntimeException("Member not found"));
         item.setMember(member);
 
-        String imagePath = saveImageFile(imageFile);
-        if (imagePath != null) {
-            String mimeType = determineMimeType(imagePath);
-            String base64EncodedImage = encodeImageToBase64(imagePath);
+        if (!imageFile.isEmpty()) {
+            try {
+                String mimeType = imageFile.getContentType(); // MIME 타입 추출
+                String base64EncodedImage = resizeAndSaveImage(imageFile);
 
-            item.setImagePath(base64EncodedImage);
-            item.setMimeType(mimeType); // MIME 타입 저장
+                item.setImagePath(base64EncodedImage);
+                item.setMimeType(mimeType); // 추출한 MIME 타입 저장
+            } catch (IOException e) {
+                log.error("Error resizing and encoding image: {}", e.getMessage(), e);
+            }
         }
+
         return itemRepository.save(item);
     }
+
 
     @Override
     public void update(Long itemId, ItemUpdateDto updateParam, MultipartFile imageFile, Long currentMemberId) {
         Item existingItem = itemRepository.findById(itemId)
                 .orElseThrow(() -> new RuntimeException("Item not found"));
-        // 소유권 확인
         if (!existingItem.getMember().getId().equals(currentMemberId)) {
             throw new RuntimeException("Unauthorized access");
         }
 
         if (!imageFile.isEmpty()) {
-            // 새 이미지 파일 저장 및 Base64 인코딩과 MIME 타입 업데이트
-            String imagePath = saveImageFile(imageFile);
-            if (imagePath != null) {
-                String mimeType = determineMimeType(imagePath);
-                String base64EncodedImage = encodeImageToBase64(imagePath);
+            try {
+                String mimeType = imageFile.getContentType(); // MIME 타입 추출
+                String base64EncodedImage = resizeAndSaveImage(imageFile);
 
                 updateParam.setImagePath(base64EncodedImage);
                 updateParam.setMimeType(mimeType); // 새로운 MIME 타입 설정
+            } catch (IOException e) {
+                log.error("Error resizing and encoding image: {}", e.getMessage(), e);
             }
         } else {
-            // 새 이미지 파일이 없으면, 기존 이미지 정보 유지
             updateParam.setImagePath(existingItem.getImagePath());
-            updateParam.setMimeType(existingItem.getMimeType()); // 기존 MIME 타입 유지
+            updateParam.setMimeType(existingItem.getMimeType());
         }
 
         itemRepository.update(itemId, updateParam);
     }
+
 
     @Override
     public Optional<Item> findById(Long id) {
@@ -100,58 +102,13 @@ public class ItemServiceV1 implements ItemService {
         itemRepository.deleteById(item.getId());
     }
 
-    private String saveImageFile(MultipartFile imageFile) {
-        if (imageFile.isEmpty()) {
-            return null;
-        }
-
-        try {
-            String directoryPath = "/home/baek/Downloads/project/src/main/resources/static/images";
-            File directory = new File(directoryPath);
-            if (!directory.exists()) {
-                directory.mkdirs();
-            }
-
-            String originalFilename = imageFile.getOriginalFilename();
-            String storedFileName = System.currentTimeMillis() + "_" + originalFilename;
-
-            // 여기에서 이미지 크기 조정 및 저장
-            return resizeAndSaveImage(imageFile, directoryPath, storedFileName);
-        } catch (IOException e) {
-            log.error("Error processing image file: {}", e.getMessage(), e);
-            return null;
-        }
-    }
-
-    private String encodeImageToBase64(String imagePath) {
-        try {
-            Path path = Paths.get(imagePath);
-            byte[] imageBytes = Files.readAllBytes(path);
-            return Base64.getEncoder().encodeToString(imageBytes);
-        } catch (IOException e) {
-            log.error("Error encoding image to Base64: {}", imagePath, e);
-            return null;
-        }
-    }
-
-    private String determineMimeType(String imagePath) {
-        try {
-            Path path = Paths.get(imagePath);
-            return Files.probeContentType(path);
-        } catch (IOException e) {
-            log.error("Error determining MIME type: {}", imagePath, e);
-            return null;
-        }
-    }
 
     /**
      * base64 인코딩 하기 전 사이즈를 줄이는 함수 (사이즈가 크면 타임리프에서 출력 X)
      */
-    private String resizeAndSaveImage(MultipartFile imageFile, String directoryPath, String storedFileName) throws IOException {
-        // 이미지 파일 읽기
+    private String resizeAndSaveImage(MultipartFile imageFile) throws IOException {
         BufferedImage originalImage = ImageIO.read(imageFile.getInputStream());
 
-        // 이미지 크기 조정 (예: 너비 500, 높이 500)
         int targetWidth = 500;
         int targetHeight = 500;
         BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
@@ -159,12 +116,10 @@ public class ItemServiceV1 implements ItemService {
         graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
         graphics2D.dispose();
 
-        // 조정된 이미지 저장
-        Path filePath = Paths.get(directoryPath, storedFileName);
-        ImageIO.write(resizedImage, "jpg", filePath.toFile()); // "jpg"는 이미지 형식에 따라 변경
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(resizedImage, "jpg", baos);
+        byte[] imageBytes = baos.toByteArray();
 
-        return filePath.toString();
+        return Base64.getEncoder().encodeToString(imageBytes);
     }
-
-
 }
